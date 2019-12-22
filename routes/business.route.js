8d3b36ed3b7065d82b4f18678a81dd4c4430953e
vimal kovath <vimal.k@fulfillsolutions.com>
2019-12-22 08:10:42 +0530

    init

---
+++ routes/business.route.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const app = express();
+const businessRoutes = express.Router();
+
+// Require Business model in our routes module
+let Business = require('../models/Business');
+
+// Defined store route
+businessRoutes.route('/add').post(function (req, res) {
+  let business = new Business(req.body);
+  Business.find({person_name:business.person_name},function (err,docs)
+  {
+    if(docs.length)
+    {
+      console.log("name existed");
+        res.json({"messgaes":"name already-exited"});
+      }
+    else
+    {
+      business.save()
+      .then(business => {
+        // res.status(200).json({'business': 'business in added successfully'});
+        res.json(business);
+        console.log(business);
+      })
+      .catch(err => {
+      res.status(400).send("unable to save to database");
+      });
+    }
+  });
+});
+
+// Defined get data(index or listing) route
+businessRoutes.route('/').get(function (req, res) {
+    Business.find(function (err, businesses){
+    if(err){
+      console.log(err);
+    }
+    else {
+      res.json(businesses);
+    }
+  });
+});
+
+// // Defined edit route
+// businessRoutes.route('/edit/:id').get(function (req, res) {
+//   let id = req.params.id;
+//   Business.findById(id, function (err, business){
+//       res.json(business);
+//   });
+// });
+
+//  Defined update route
+businessRoutes.route('/update/:id').post(function (req, res) {
+    Business.findById(req.params.id, function(err, business) {
+    if (!business)
+      return next(new Error('Could not load Document'));
+    else {
+        business.person_name = req.body.person_name;
+        business.amount = req.body.amount;
+        // business.business_gst_number = req.body.business_gst_number;
+
+        business.save().then(business => {
+          res.json('Update complete');
+      })
+      .catch(err => {
+            res.status(400).send("unable to update the database");
+      });
+    }
+  });
+});
+
+// Defined delete | remove | destroy route
+businessRoutes.route('/delete/:id').get(function (req, res) {
+    Business.findByIdAndRemove({_id: req.params.id}, function(err, business){
+        if(err) res.json(err);
+        else res.json('Successfully removed');
+    });
+});
+
+module.exports = businessRoutes;
--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var mongoose = require('mongoose');
+var config = require('./DB');
+	
 var app = express();
 
+app.use(bodyParser.json());
+app.use(cors());
+
+var businessRoute = require('./routes/business.route');
+mongoose.Promise = global.Promise;
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8080;
+
+mongoose.connect(config.DB, { useNewUrlParser: true }).then(
+	() => {console.log('Database is connected') },
+	err => { console.log('Cannot connect to the database'+ err)}
+  );
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
@@ -18,6 +33,21 @@ app.get('/', function(req, res) {
 	res.render('index');
 });
 
+
+app.use('/business', businessRoute);
+
+
 app.listen(port, function() {
 	console.log('Our app is running on http://localhost:' + port);
 });
+
+
+
+
+
+
+
+
+
+
+
